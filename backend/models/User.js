cf4cf74d49fcfd301150833f289b565d46126c1f
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2025-07-16 21:10:09 +0530

    update

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,12 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: {
-      type: String,
-      enum: ['Admin', 'User'],
-      default: 'User',
-      required: true,
-    },
+    // role field removed
   },
   { timestamps: true }
 );
